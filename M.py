10be265130528783b1f17c7af26405ec5a1693d8
Black on <153239300+B-lackon@users.noreply.github.com>
2026-04-30 22:59:26 +0300

    Update BOT_TOKEN and BOT_OWNER values

--- M.py
+++ M.py
@@ -34,8 +34,8 @@ try:
 except:
     SIGNERPY_AVAILABLE = False
 
-BOT_TOKEN = input("- Enter your Token : ").strip()
-BOT_OWNER = 8278578545
+BOT_TOKEN = input("- 6557143211:AAEx1P75HQ1HxMdYl0tTUZbEHXnSsyMF8ao : ").strip()
+BOT_OWNER = 1997851407
 bot = telebot.TeleBot(BOT_TOKEN)
 
 user_states = {}
